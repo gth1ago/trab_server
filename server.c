9cc386e73653cb6a28063801b166d5292b5c9cb9
GTh1ago <gabriel.thiagohs@gmail.com>
2021-05-11 17:12:38 -0300

    rename

--- server.c
+++ server.c
@@ -5,7 +5,7 @@
 #include<arpa/inet.h>	//inet_addr
 #include<unistd.h>	//write
 #include<pthread.h> //for threading , link with lpthread
-#include "testeXML.c"
+#include "functionsXML.c"
 
 // voce pode usar para extrair as informações a 
 // biblioteca libxml(http://www.xmlsoft.org/index.html) 
